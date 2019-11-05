cac278f53a172bb3a907476051c6058d23bc486d
DanielMartinezA <54421405+DanielMartinezA@users.noreply.github.com>
2019-11-05 17:46:23 -0300

    ec_dif y actualizacion al script de QR

--- ob2/codigoQR.m
+++ ob2/codigoQR.m
@@ -17,34 +17,64 @@ Y1_cv = log(Y1);
 Y2_cv = log(Y2);
 
 A = [X_cv ones(m,1)];
-[Q, R] = qr(A);
-R1 = R(1:n,:);% 1eras n filas de R
+% Descomposicion QR de A:
+% [Q, R] = qr(A);
+% Resulta mejor calcular Q_r y R_r directamente
+% Q = [Q_r Q2]
+Q_r = zeros(m,n);
+Q_r(:,1) = A(:,1)/norm(A(:,1));
+for i = 2:n;
+    aux = A(:,i);
+    for j = 1:(i-1);
+        aux = aux - dot(A(:,i),Q_r(:,j))*Q_r(:,j);
+    end
+    Q_r(:,i) = aux/norm(aux);
+end
 
+% R = [   R_r  ]
+%     [0 0 .. 0]
+% R_r = R(1:n,:);% 1eras n filas de R
+R_r = Q_r'*A;
+% Los elementos fuera del triangulo quedan en x10^-17 etc, entonces los hago cero.
+for i = 1:n;
+    for j = 1:(i-1);
+        R_r(i,j) = 0;
+    end
+end
 %--------------
 % Funcion g1: %
 %--------------
-QtY1_1 = (Q'*Y1_cv)(1:n); % 1eros n elementos del vector Q'Y
+% QtY1_1 = (Q'*Y1_cv)(1:n); % 1eros n elementos del vector Q'Y
+QtY1_1 = Q_r'*Y1_cv;
 
 % resuelvo el sist lineal R1X = (Q'Y)_1 con R1 matriz triangular superior, resolver con sustitucion hacia atras.
 ec1_opt.UT = true;
-Param_1 = linsolve(R1,QtY1_1,ec1_opt);
+Param_1 = linsolve(R_r,QtY1_1,ec1_opt);
 % solucion es y = X(1).t + X(2)
 % y = c.x^(-p) ; log(y) = -p.log(x) + log(c) ; y_cv = A.t + B
-p1 = -Param_1(1);
-c1 = exp(Param_1(2));
+p = round(-Param_1(1));
+c = exp(Param_1(2));
+
+% Valor minimizado: ||AX - Y||^2
+dist_g1 = sum((c*X.^(-p) - Y1).^2);
 
 %--------------
 % Funcion g2: %
 %--------------
-QtY1_2 = (Q'*Y2_cv)(1:n); % 1eros n elementos del vector Q'Y
+% QtY1_2 = (Q'*Y2_cv)(1:n); % 1eros n elementos del vector Q'Y
+QtY1_2 = Q_r'*Y2_cv;
 
 % resuelvo el sist lineal R1X = (Q'Y)_1 con R1 matriz triangular superior, resolver con sustitucion hacia atras.
 ec2_opt.UT = true;
-Param_2 = linsolve(R1,QtY1_2,ec2_opt);
+Param_2 = linsolve(R_r,QtY1_2,ec2_opt);
 % solucion es y = X(1).t + X(2)
-% y = c.x^(-p) ; log(y) = -p.log(x) + log(c) ; y_cv = A.t + B
-p2 = -Param_2(1);
-c2 = exp(Param_2(2));
+% y = d.x^(-q) ; log(y) = -q.log(x) + log(d) ; y_cv = A.t + B
+q = round(-Param_2(1));
+d = exp(Param_2(2));
+
+% Valor minimizado: ||AX - Y||^2
+dist_g2 = sum((d*X.^(-q) - Y2).^2);
+
 %---------------------------------------
 % Graficas para correccion del modelo: %
 %---------------------------------------
@@ -55,19 +85,17 @@ plot(X_cv, Y1_cv, 'bs')
 hold on
 vect_Xcv = linspace(X_cv(1),X_cv(m),50);
 plot(vect_Xcv, Param_1(1)*vect_Xcv + Param_1(2), 'r')
-param_polyfit_1 = polyfit(X_cv,Y1_cv,1);
-plot(vect_Xcv, param_polyfit_1(1)*vect_Xcv + param_polyfit_1(2), 'm')
 title ("Funcion g1 - lineal");
 xlabel ("x");
 ylabel ("y");
-legend("datos transformados", "ajuste QR", "ajuste polyfit");
+legend("datos transformados", "ajuste QR");
 
 % Grafica sin transformacion logaritmica de los datos
 figure (2)
 plot(X, Y1, 'bs')
 hold on
 vect_X = linspace(X(1),X(m),50);
-plot(vect_X, c1*vect_X.^(-p1), 'r')
+plot(vect_X, c*vect_X.^(-p), 'r')
 title ("Funcion g1 - original");
 xlabel ("x");
 ylabel ("y");
@@ -80,19 +108,17 @@ plot(X_cv, Y2_cv, 'bs')
 hold on
 % vect_Xcv = linspace(X_cv(1),X_cv(m),50);
 plot(vect_Xcv, Param_2(1)*vect_Xcv + Param_2(2), 'r')
-param_polyfit_2 = polyfit(X_cv,Y2_cv,1);
-plot(vect_Xcv, param_polyfit_2(1)*vect_Xcv + param_polyfit_2(2), 'm')
 title ("Funcion g2 - lineal");
 xlabel ("x");
 ylabel ("y");
-legend("datos transformados", "ajuste QR", "ajuste polyfit");
+legend("datos transformados", "ajuste QR");
 
 % Grafica sin transformacion logaritmica de los datos
 figure (4)
 plot(X, Y2, 'bs')
 hold on
 % vect_X = linspace(X(1),X(m),50);
-plot(vect_X, c2*vect_X.^(-p2), 'r')
+plot(vect_X, d*vect_X.^(-q), 'r')
 title ("Funcion g2 - original");
 xlabel ("x");
 ylabel ("y");
---
+++ ob2/ec_diferenciales.m
@@ -0,0 +1,78 @@
+% Funcion g1(x) = c/x^p
+c = 0.96750; % con outlier
+p = 2; % con outlier
+% [c p] = [2 0.94613] sin outlier
+
+% Funcion g2(x) = d/x^q
+d = 1.9949;
+q = 3;
+
+% EDO:
+% y' = g1(x)*y = g2(x)
+% y(0.5) = 0
+% x in [0.5 .. 2]
+
+a = 0.5;
+b = 2;
+y0 = 0;
+% calculo paso h dentro de region de estabilidad.
+h = 0.015 ; % VALOR PROVISORIO %
+N = ceil((b-a)/h);
+% Recalculo h
+h = (b-a)/N;
+
+x_exacta = linspace(a,b,100);
+% sol analitica con p = 2 y q = 3:
+k = (-d/c)*(1/c + 2)*exp(-2*c); % obtenido con la cond inicial.
+y_exacta = d/c^2 + d./(c*x_exacta) + k*exp(c./x_exacta);
+
+% Euler hacia atras:
+x = linspace(a,b,N);
+y_EA = zeros(length(x),1);
+y_EA(1) = y0;
+for i = 2:length(x);
+    y_EA(i) = (y_EA(i-1) + d*h/x(i)^3)/(1 + c*h/x(i)^2);
+    %y_EA(i) = ( x(i)^2*y_EA(i-1) + d*h/x(i) )/(x(i)^2 + c*h); % otra forma
+end
+
+% Euler hacia adelante:
+% usa el mismo x que euler hacia atras
+y_E = zeros(length(x),1);
+y_E(1) = y0;
+for i = 2:length(x);
+    y_E(i) = y_E(i-1) + h*(-c*y_E(i-1)/x(i-1)^2 + d/x(i-1)^3);
+end
+
+% Runge-Kuta con ode solver ode45()
+function ans = ecdif(x,y)
+    c = 0.96140;
+    d = 1.9598;
+    ans = -c*y/x^2 + d/x^3;
+end
+
+[x_RK, y_RK] = ode45(@ecdif, [a b], y0);
+
+% Graficas comparativas de Euler hacia atras, Euler hacia adelante, R-K y sol analitica:
+figure(1)
+%Sol analitica
+plot(x_exacta, y_exacta, "b")
+hold on
+% Euler hacia adelante
+plot(x, y_E, "r")
+% Euler hacia atras
+plot(x, y_EA, "m")
+% Runge-Kuta
+plot(x_RK, y_RK, "k")
+
+axis ([a b])
+title ("Sol de la EDO");
+xlabel ("x");
+ylabel ("y");
+%legend("Sol Analitica", "Euler hacia adelante", "Euler hacia atras", "Runge-Kuta");
+hold off
+legend_text = legend ("Sol Analitica", "Euler hacia adelante", "Euler hacia atras", "Runge-Kuta");
+legend (legend_text, "location", "southeast");
+% CAMBIOS A REALIZAR:
+% Forzar el rango de la grafica en [a b].
+% Cambiar la posicion de la leyenda a la esquina inferior derecha.
+% -----------------------------------------------------------------------------------
